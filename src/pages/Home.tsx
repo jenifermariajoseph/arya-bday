136d31fcc5cfb15a1c93792fcf587e13805c4575
jenifermariajoseph <jenifermj4403@gmail.com>
2025-08-24 07:32:40 +0530

    Initial commit: Digital birthday card with interactive animations and personalized message

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import BirthdayCard from '@/components/BirthdayCard';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <BirthdayCard />
+    </div>
+  );
+}
